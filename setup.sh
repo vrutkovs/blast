c1bf950b2db76474c1dd5e24e9db73c198b3892a
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-05-07 22:39:24 +0200

    Update openshift configs and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-#UI
-oc new-app \
-    soltysh/lighttpd-centos7~https://github.com/vrutkovs/catcatgo.git \
-    --context-dir=ui \
-    --name=ui \
-    --labels=app=ui
+oc new-project catcatgo
 
-oc set probe deploymentconfig/ui --liveness --open-tcp=8080
-oc set probe deploymentconfig/ui --readiness --get-url=http://:8080/
+oc create -f is-lighttpd-centos7.yml
+oc create -f is-backend.yml
+oc create -f is-scraper.yml
+oc create -f is-ui.yml
 
-# create service account for the cron job
-oc create serviceaccount scraper
-oc policy add-role-to-user edit -z scraper
-oc run scraper --schedule="0/5 * * * *" --image=soltysh/scraper --restart=Never
+oc create -f confimap-db.yml
+oc create -f confimap-scraper.yml
+oc create -f rolebinding-edit.yml
 
-# create all the templates for each component
-for dir in $(find * -maxdepth 0 -type d)
-do
-    oc new-app ${dir}/template.yaml
-done
+oc create -f bc-backend.yml
+oc create -f bc-scraper.yml
+oc create -f bc-ui.yml
 
-# expose the app
-oc expose service/ui
+oc create -f pvc-database.yml
+oc create -f dc-database.yml
+oc create -f svc-database.yml
+
+oc create -f dc-backend.yml
+oc create -f svc-backend.yml
+oc create -f route-backend.yml
+
+oc create -f dc-ui.yml
+oc create -f svc-ui.yml
+oc create -f route-ui.yml
+
+oc create -f cronjob-scraper.yml
