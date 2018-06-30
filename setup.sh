a80d91d93a544cadba2f7969e8999d2315d09a22
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-06-30 14:47:58 +0200

    setup.sh: use apply instead of create

--- setup.sh
+++ setup.sh
@@ -4,37 +4,37 @@ set -eux
 # Create catcatgo app bits
 oc new-project catcatgo || oc project catcatgo
 
-oc create -f openshift/is-lighttpd-centos7.yml
-oc create -f openshift/is-backend.yml
-oc create -f openshift/is-scraper.yml
-oc create -f openshift/is-ui.yml
+oc apply -f openshift/is-lighttpd-centos7.yml
+oc apply -f openshift/is-backend.yml
+oc apply -f openshift/is-scraper.yml
+oc apply -f openshift/is-ui.yml
 
-oc create -f openshift/confimap-db.yml
-oc create -f openshift/confimap-scraper.yml
-oc create -f openshift/rolebinding-edit.yml
+oc apply -f openshift/confimap-db.yml
+oc apply -f openshift/confimap-scraper.yml
+oc apply -f openshift/rolebinding-edit.yml
 
-oc create -f openshift/bc-backend.yml
-oc create -f openshift/bc-scraper.yml
-oc create -f openshift/bc-ui.yml
+oc apply -f openshift/bc-backend.yml
+oc apply -f openshift/bc-scraper.yml
+oc apply -f openshift/bc-ui.yml
 
 oc adm policy add-scc-to-user anyuid -z default
 oc adm policy add-scc-to-user privileged -z default
 
-oc create -f openshift/pvc-database.yml
+oc apply -f openshift/pvc-database.yml
 oc apply -f <(istioctl kube-inject -f openshift/dc-database.yml)
-oc create -f openshift/svc-database.yml
+oc apply -f openshift/svc-database.yml
 
 oc apply -f <(istioctl kube-inject -f openshift/dc-backend.yml)
-oc create -f openshift/svc-backend.yml
+oc apply -f openshift/svc-backend.yml
 
 oc apply -f <(istioctl kube-inject -f openshift/dc-ui.yml)
-oc create -f openshift/svc-ui.yml
+oc apply -f openshift/svc-ui.yml
 
-oc create -f openshift/cronjob-scraper.yml
+oc apply -f openshift/cronjob-scraper.yml
 
 # Add istio rules
-oc create -f istio/route-rules.yml
-oc create -f istio/gateway.yml
+oc apply -f istio/route-rules.yml
+oc apply -f istio/gateway.yml
 
 oc delete route istio-ingressgateway -n istio-system
 oc create route edge --service=istio-ingressgateway --hostname=catcatgo.cloud.vrutkovs.eu -n istio-system
