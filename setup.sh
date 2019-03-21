9786e96d1e2d6d01109939b9c7f6e61413bf3266
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-03-21 10:07:36 +0100

    Create files from openshift folder

--- setup.sh
+++ setup.sh
@@ -2,29 +2,29 @@
 
 oc new-project catcatgo
 
-oc create -f is-lighttpd-centos7.yml
-oc create -f is-backend.yml
-oc create -f is-scraper.yml
-oc create -f is-ui.yml
+oc create -f openshift/is-lighttpd-centos7.yml
+oc create -f openshift/is-backend.yml
+oc create -f openshift/is-scraper.yml
+oc create -f openshift/is-ui.yml
 
-oc create -f confimap-db.yml
-oc create -f confimap-scraper.yml
-oc create -f rolebinding-edit.yml
+oc create -f openshift/confimap-db.yml
+oc create -f openshift/confimap-scraper.yml
+oc create -f openshift/rolebinding-edit.yml
 
-oc create -f bc-backend.yml
-oc create -f bc-scraper.yml
-oc create -f bc-ui.yml
+oc create -f openshift/bc-backend.yml
+oc create -f openshift/bc-scraper.yml
+oc create -f openshift/bc-ui.yml
 
-oc create -f pvc-database.yml
-oc create -f dc-database.yml
-oc create -f svc-database.yml
+oc create -f openshift/pvc-database.yml
+oc create -f openshift/dc-database.yml
+oc create -f openshift/svc-database.yml
 
-oc create -f dc-backend.yml
-oc create -f svc-backend.yml
-oc create -f route-backend.yml
+oc create -f openshift/dc-backend.yml
+oc create -f openshift/svc-backend.yml
+oc create -f openshift/route-backend.yml
 
-oc create -f dc-ui.yml
-oc create -f svc-ui.yml
-oc create -f route-ui.yml
+oc create -f openshift/dc-ui.yml
+oc create -f openshift/svc-ui.yml
+oc create -f openshift/route-ui.yml
 
-oc create -f cronjob-scraper.yml
+oc create -f openshift/cronjob-scraper.yml
